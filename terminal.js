391c2755c7516c1ad312fe413d15bf9d824923ec
superseva <superseva@gmail.com>
2020-09-23 12:34:39 +0200

    need sockets

--- terminal.js
+++ terminal.js
@@ -1,3 +1,10 @@
+
+CONFIG.FoTerminal = {
+  "init":false
+};
+
+
+
 class FalloutTerminal{
 
   static ACTIVATE_TERMINAL_CMD = '!_activate_terminal';
@@ -18,7 +25,6 @@ class FalloutTerminal{
     this.curScreenId;
   }
 
-
   /**
    * Activates a terminal by initializing its JSON and displaying the
    * first screen of the terminal.
@@ -26,12 +32,12 @@ class FalloutTerminal{
    * @param  {string} id
    */
   activateTerminal(_journalName) {
+    this.curTerminal = {};
+    this.nextItemId = 0;
+    this.history = [];
+    this.curScreenId;
 
-    Hooks.once('renderChatMessage', (message, html, data) => {
-      this._checkChatMessage(message.data, html,data);
-    });
     var json = this._getTerminalJson(_journalName);
-    //console.log(json);
     if(json) {
       this._initTerminal(json);
       this._displayScreen(this.curTerminal._startId);
@@ -65,7 +71,7 @@ class FalloutTerminal{
     this.history = [];
 
     this._initTerminalScreens(json);
-    //console.log(json);
+
     $.extend(this.curTerminal, {
       _locked: json.locked,
       _password: json.password,
@@ -73,8 +79,6 @@ class FalloutTerminal{
       _startId: json.id,
       _terminalName: json.name
     });
-
-    //console.log(this.curTerminal);
   }
 
   /**
@@ -93,10 +97,7 @@ class FalloutTerminal{
     // Recursively create the child items' IDs.
     screen.screenIds = [];
     if(screen.screens!=undefined){
-      //console.log(screen.screens);
       var l = screen.screens.length;
-      //console.log(screen.screens.length);
-
       for ( var i = 0; i < screen.screens.length; i++ ) {
         var child = screen.screens[i];
 
@@ -121,15 +122,17 @@ class FalloutTerminal{
     var screen = this.curTerminal[id];
     this.curScreenId = id;
 
-    var html = '<table>';
-    html += '<thead><tr><th>' + this.curTerminal._terminalName + '</th></tr>';
+    var html = '<table style="background-color: ' + this._getBgColor() + '; border: solid 1px ' + this._getBgColor() + '; border-collapse: separate; border-radius: 10px; font-family: monospace; overflow: hidden; width: 100%;">';
+    html += '<thead><tr><th style="color: '+this._getTextColor()+'">' + this.curTerminal._terminalName + '</th></tr>';
     html += '</thead>';
     html += '<tbody>';
-    html += '<tr><td>';
-    if(screen)
+    html += '<tr><td style ="color: ' + this._getTextColor() + '; padding: 0.5em;">';
+    if(screen){
       html += this._displayScreenContent(screen);
-    else
+    }
+    else{
       html += this._displayScreenContent('ERROR 0xFFFFF710\n"Data Corru:xsfkleg,, g364[735}3__' + id + '."');
+    }
 
      if(!this.curTerminal._locked){
        html += this._displayScreenButtons(screen);
@@ -147,12 +150,11 @@ class FalloutTerminal{
    * @return {string}
    */
   _displayScreenButton(cmd, label, id) {
-    return '<button class="terminal-chat-button" data-label="'+label+'" data-screenid="'+id+'">'+ label +'</button>'
+    return '<button style="background: '+ this._getBgColor()+ '; color: '+this._getTextColor()+'; border: none; margin-bottom:0.2em;" class="terminal-chat-button" data-label="'+label+'" data-screenid="'+id+'">'+ label +'</button>'
      // return '<a href="' + cmd + '" style="background: ' + this._getButtonColor() + '; border: none; color: ' + this._getTextColor() + '; margin: 0.2em 0; width: 95%;">' + label + '</a>';
   }
   _displayBackButton() {
-    return '<div style="text-align:center;" class="terminal-back-button">BACK</div>';
-     // return '<a href="' + cmd + '" style="background: ' + this._getButtonColor() + '; border: none; color: ' + this._getTextColor() + '; margin: 0.2em 0; width: 95%;">' + label + '</a>';
+    return '<button class="terminal-back-button" style="background: '+ this._getBgColor()+ '; color: '+this._getTextColor()+'; border: none; margin-bottom:0.2em;">BACK</button>';
   }
 
   /**
@@ -193,11 +195,13 @@ class FalloutTerminal{
       html += this._htmlNewlines(screen);
     }
     else if(this.curTerminal._locked) {
-      if(!screen.attempts)
+      //if(!screen.attempts){
+      if(screen.attempts<1){
         html += this._htmlNewlines('TERMINAL LOCKED\n\nPLEASE CONTACT AN ADMINISTRATOR');
-      else
-        html += this._htmlNewlines('ENTER PASSWORD');
-      //  html += this._htmlNewlines(this._displayScreenHacking());
+      }
+      else{
+        html += this._htmlNewlines(this._displayScreenHacking());
+      }
     }
     else {
       html += this._htmlNewlines(screen.name + '\n\n' + (screen.content || ''));
@@ -207,7 +211,32 @@ class FalloutTerminal{
   }
 
   _displayScreenHacking(){
+    var attempts = this.curTerminal._attempts;
+    //if(!attempts)
+    if(attempts<1){
+      return 'TERMINAL LOCKED\n\nPLEASE CONTACT AN ADMINISTRATOR';}
+    else {
+      var html = attempts + ' ATTEMPT(S) LEFT: \n';
+      html += '\n\n';
+      for (var n = 0; n < attempts; ++ n){
+        html += '&#9608; ';
+      }
 
+      // Display a fake RAM dump.
+      var startAddr = 0xf000 + Math.floor(Math.random()*0xf0f);
+      var inc = 10;
+      for(var r=0;r<16;r++){
+        var addr = startAddr + inc*i;
+        html += '0x' + addr.toString(16).toUpperCase() + ' ';
+        for(var i =0;i<inc;i++){
+          html += FalloutTerminal.ASCII_TABLE[Math.floor(Math.random()*FalloutTerminal.ASCII_TABLE.length)].replace('<','&lt;').replace('>', '&gt;');
+        }
+      }
+      html += '\n\n';
+
+      html += 'ENTER PASSWORD by typing \"!pass [password]\"'
+      return html;
+    }
   }
   /**
    * Guesses the password for the terminal.
@@ -231,7 +260,7 @@ class FalloutTerminal{
   }
 
   async _sendChat(name, content){
-    console.log("CREATING MESSAGE");
+
     await ChatMessage.create({ content: content });
     // Hooks.once('renderChatMessage', (message, html, data) => {
     //   this._checkChatMessage(message.data, html,data);
@@ -240,48 +269,45 @@ class FalloutTerminal{
   }
 
   _onTerminalButtonScreen($el){
-    console.log('ALOOOO');
-    console.log($el.currentTarget.dataset.screenid);
+    $($el.data._html).find(".terminal-chat-button").off();
+    $($el.data._html).find(".terminal-back-button").off();
     var id = $el.currentTarget.dataset.screenid;
     this.history.push(this.curScreenId);
     this._displayScreen(id);
   }
   _onTerminalButtonBack($el){
+    $($el.data._html).find(".terminal-chat-button").off();
+    $($el.data._html).find(".terminal-back-button").off();
     var id = this.history.pop();
     this._displayScreen(id);
   }
 
   _checkChatMessage(msg, html,data){
-    $(html).find(".terminal-chat-button").click(this._onTerminalButtonScreen.bind(this));
-    $(html).find(".terminal-back-button").click(this._onTerminalButtonBack.bind(this));
-    try {
-      if(msg.content === FalloutTerminal.ACTIVATE_TERMINAL_CMD && msg.selected) {
-        this._activateTerminal(msg.selected[0]._id);
-      }
-      if(msg.content.indexOf(FalloutTerminal.SHOW_SCREEN_CMD) === 0) {
-        var args = msg.content.split(' ');
-        var id = args[1];
-        this.history.push(this.curScreenId);
-        this._displayScreen(args[1]);
-        //console.log(args[1]);
-      }
-      if(msg.content.indexOf(FalloutTerminal.PREV_SCREEN_CMD) === 0) {
-        var id = this.history.pop();
-        this._displayScreen(id);
-      }
-      if(msg.content.indexOf(FalloutTerminal.PASSWORD_CMD) === 0) {
-        var args = msg.content.split(' ');
-        var password = args.slice(1).join(' ');
-        this._guessPassword(password);
-      }
-    }
-    catch(err) {
-      console.log('FALLOT TERMINAL ERROR: ' + err.message);
-    }
-
-
-
-
+    $(html).find(".terminal-chat-button").click({_html:html},this._onTerminalButtonScreen.bind(this));
+    $(html).find(".terminal-back-button").click({_html:html},this._onTerminalButtonBack.bind(this));
+    //try {
+      // if(msg.content === FalloutTerminal.ACTIVATE_TERMINAL_CMD && msg.selected) {
+      //   this._activateTerminal(msg.selected[0]._id);
+      // }
+      // if(msg.content.indexOf(FalloutTerminal.SHOW_SCREEN_CMD) === 0) {
+      //   var args = msg.content.split(' ');
+      //   var id = args[1];
+      //   this.history.push(this.curScreenId);
+      //   this._displayScreen(args[1]);
+      // }
+      // if(msg.content.indexOf(FalloutTerminal.PREV_SCREEN_CMD) === 0) {
+      //   var id = this.history.pop();
+      //   this._displayScreen(id);
+      // }
+      // if(msg.content.indexOf(FalloutTerminal.PASSWORD_CMD) === 0) {
+      //   var args = msg.content.split(' ');
+      //   var password = args.slice(1).join(' ');
+      //   this._guessPassword(password);
+      // }
+    // }
+    // catch(err) {
+    //   console.log('FALLOT TERMINAL ERROR: ' + err.message);
+    // }
 
   }
 
@@ -292,6 +318,34 @@ class FalloutTerminal{
   _getTextColor(){
     return FalloutTerminal.DEFAULT_TEXT_COLOR;
   }
+  _getBgColor(){
+    return FalloutTerminal.DEFAULT_BG_COLOR;
+  }
 
+}
 
+var falloutTerminal;
+Hooks.on("ready", function(){
+  console.warn(CONFIG);
+  console.warn("IS INIT ", CONFIG.FoTerminal);
+  if(!CONFIG.FoTerminal.init)
+    CONFIG.FoTerminal.init = true;
+  console.warn("IS INIT ", CONFIG.FoTerminal);
+  initTerminals();
+});
+
+function initTerminals(){
+  falloutTerminal = new FalloutTerminal();
+  Hooks.on('renderChatMessage', (message, html, data) => {
+      falloutTerminal._checkChatMessage(message.data, html,data);
+  });
+
+  Hooks.on('preCreateChatMessage', (msg, content)=>{
+    if(msg.content.indexOf(FalloutTerminal.PASSWORD_CMD) === 0) {
+      var args = msg.content.split(' ');
+      var password = args.slice(1).join(' ');
+      falloutTerminal._guessPassword(password);
+      return false;
+    }
+  });
 }
